ee83e51b482fd37f8f958bfdec5e294c293dd31c
arfataj <arfatajn3@gmail.com>
2024-09-28 16:37:37 +0530

    HAR

---
+++ HAR-Android-App/app/src/test/java/dspanah/sensor_based_har/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package dspanah.sensor_based_har;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
